e323a06fc99b8c3836b26c7e75d102de184a428e
Xavier Agache <xav.agache@gmail.com>
2022-12-08 08:56:47 +0100

    Création du repository

---
+++ src/main/java/com/example/ecf_back_localib/EcfBackLocalibApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecf_back_localib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfBackLocalibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcfBackLocalibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecf_back_localib/EcfBackLocalibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecf_back_localib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcfBackLocalibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
